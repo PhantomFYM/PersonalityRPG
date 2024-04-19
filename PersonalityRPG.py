ed8938974b521c210fbd3e4223827f1a2fba107d
Judah McBee <65314651+PhantomFYM@users.noreply.github.com>
2024-04-19 14:50:15 -0500

    Update PersonalityRPG.py
    Shrunk personality code, added field code and Stat menu.

--- PersonalityRPG.py
+++ PersonalityRPG.py
@@ -14,6 +14,23 @@ monStats = {
 "ATK": 0,
 }
 
+#Boss monster stats
+monStatMap = {
+1: (45, 0, 0), #Enemy1
+2: (45, 0, 0), #Enemy2
+3: (45, 0, 0), #Enemy3
+4: (45, 0, 0), #Enemy4
+5: (45, 0, 0), #Enemy5
+}
+
+miniMonStatMap = {
+1: (10, 0, 0),
+2: (10, 0, 0),
+3: (10, 0, 0),
+4: (10, 0, 0),
+5: (10, 0, 0),
+}
+
 #Agreement Meters
 meters = {
 "sDisagree": 0,
@@ -31,17 +48,32 @@ eResults = {}
 
 #Town texts
 towns = {
-    1: {
-        "Bob": "Hello Traveler! Here is info.",
-        "Louis": "Hello, be careful there are monsters around.",
-        "Sarah": "What's up."
-        },
-    2: {
-        "Test1": "Test words 1",
-        "Test2": "Test words 2",
-        "Test3": "Test words 3"
-        },
-    }
+1: {
+"Bob": "Hello Traveler! Here is info.",
+"Louis": "Hello, be careful there are monsters around.",
+"Sarah": "What's up."
+},
+2: {
+"Test1": "Test words 1",
+"Test2": "Test words 2",
+"Test3": "Test words 3"
+},
+3: {
+"Test1": "Test words 1",
+"Test2": "Test words 2",
+"Test3": "Test words 3"
+},
+4: {
+"Test1": "Test words 1",
+"Test2": "Test words 2",
+"Test3": "Test words 3"
+},
+5: {
+"Test1": "Test words 1",
+"Test2": "Test words 2",
+"Test3": "Test words 3"
+},
+}
     
 #Town Functions
 def town(currTown):
@@ -51,123 +83,20 @@ def town(currTown):
             print(f"[{person}] {message}")
         break
         
-#Personality Function
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4):
-                print("Your personality is INTJ, imaginative and stragetic thinkers with a plan for everything.")
 
-if(e1Result == 0): 
-    if(e2Result == 1): 
-        if(e3Result == 2): 
-            if(e4Result == 3): 
-                if(e5Result == 4): 
-                    print("Your personality is INTP, innovative investors with a lot of knowledge.")
-
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4):
-                print("Your personality is ENTJ, bold, imaginative and strong-willed leaders, always finding a way- or making one.") 
-
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4):
-                print("Your personality is ENTP, smart and curious thinkers who ca not resist an intellectual challenge.")
-
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4):
-                print("Your personality is INFJ, quiet and mystical, yet very inpiring and tireless idealist".) 
-
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4): 
-                print("Your personality is INFP, poetic, kind and altruistic people, always eager to help a good cause.") 
-
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4):
-                print("Your personality is ENFJ, charamasitc and inspiring leaders able to mesmerize their listeners.") 
-
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4):
-                print("Your personality is ENFP, euthusiastic, creative and socialable free spirits, who can always find a reason to smile.")
-
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4):
-                print("Your personality is ISTJ, practical and fact-minded individuals, whose reliability can not be doubled.") 
-
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4):
-                print("Your personality is ISFJ, very dedicated and warm protectors, always ready to defend their loved ones.") 
-
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4):
-                print("Your personality is ESTJ, excellent administrators, unsurpassed at managing things or people.")  
-
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4):
-                print("Your personality is ESFJ, caring, social, and popular people, always eager to help.") 
-
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4):
-                print("Your personality is ISTP, bold and practical experimenters") 
-
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4):
-                print("Your personality is ISFP, flexible and charming artists always ready to explore and experience something new.")
-                
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4):
-                print("Your personality is ESTP, smart, energentic and very  perceptive people.") 
-
-if(e1Result == 0):
-   if(e2Result == 1):
-    if(e3Result == 2):
-        if(e4Result == 3): 
-            if(e5Result == 4):
-                print("Your perosnality is ESFP, spontaneous, energetic and enthusiastic people.") 
 
 
 
 
 #Attack Functions
+
+
+def miniAttack():
+    print("\nYou lunge at the small monster.")
+    print("The monster takes", stats["ATK"] - monStats["Def"],"points of damage.")
+    monStats["HP"] -= (stats["ATK"] - monStats["Def"])
+    print("The monster has", monStats["HP"],"Health remaining.\n")
+    
 def Attack():
     print("\nYou lunge at the monster.")
     print("The monster takes", stats["ATK"] - monStats["Def"],"points of damage.")
@@ -216,14 +145,16 @@ def BattleEnd(currentEnemy= 2):
 
 def lvlCheck():
     if(stats["EXP"] >= stats["LVL"] ^ 2 + 5):
-        LVL += 1
+        stats["EXP"] = 0
+        stats["LVL"] += 1
         print("You Increased in level! You are now level",stats["LVL"],"!")
+    
         hpGain = stats["LVL"] * 2 + 2
         stats["HP"] += hpGain
         print("Your HP increased by",hpGain,"!")
-        DefGain = LVL + 4
-        stats["Def"] += DefGain
-        print("Your Defense increased by",DefGain,"!")
+        defGain = stats["LVL"] + 4
+        stats["Def"] += defGain
+        print("Your Defense increased by",defGain,"!")
         atkGain = stats["LVL"] + 2
         stats["ATK"] += atkGain
         print("Your Attack increased by",atkGain,"!")
@@ -257,13 +188,15 @@ def BattleScreen():
             
         if(monStats["HP"] <= 0):
             print("You slayed the monster!")
+
+
             
             if (1 >= currMonster <= 20):
                 eResults[f"e{currMonster}Result"] = BattleEnd() #f-string used to call a variable in the string, drastically shrinks
-                #if-statements. Thanks again python discord.
+                #if-statements.
                 
             xpGain = (currMonster * 10) ^ 2
-            print("You gained",xpGain,"EXP points!")
+            print("You gained",xpGain,"EXP points!\n")
             stats["EXP"] += xpGain
             lvlCheck()
             meterReset()
@@ -276,7 +209,50 @@ def BattleScreen():
             print("Game Over")
             input()
             exit()
-    
+
+def miniBattleScreen():
+    while True:
+        
+        select = input("\n[0] Attack\n[1] Run\n\n")
+        if (select.isdigit()): #Tests if select is a digit input before confirming
+            select = int(select)
+            if(select == 0):
+                miniAttack()
+                
+            elif(select == 1):
+                print("\nYou ran away.\n")
+                break
+            
+            else:
+                print("\nInvalid Input, try again.")
+            
+        else:
+            print("\nInvalid Input, try again.")
+            
+        if(monStats["HP"] <= 0):
+            print("You slayed the small monster!")
+            xpGain = (currMonster * 2)
+            print("You gained",xpGain,"EXP points!")
+            stats["EXP"] += xpGain
+            lvlCheck()
+            miniSelect = input("Continue in field? \n[0] Yes\n[1] No\n")
+            if(miniSelect.isdigit()):
+                miniSelect = int(miniSelect)
+                if(miniSelect == 0):
+                    setMonStats(*miniMonStatMap.get(currMonster, (0, 0, 0)))
+                    continue
+                if(miniSelect == 1):
+                    break
+                    
+            break
+
+        EnemyAttack()
+
+        if(stats["HP"] <= 0):
+            print("Game Over")
+            input()
+            exit()
+            
 #Monster functions    
 def setMonStats(HP, ATK, Def):
     global monStats
@@ -286,26 +262,86 @@ def setMonStats(HP, ATK, Def):
         "Def": Def,
         }
 
+
 #Area Menu
 while True:
-    print("\n[0] Town\n[1] Area\n[2] Item\n[3] Advance\n")
+    print("\n[0] Town\n[1] Area\n[2] Item\n[3] Advance\n[4] Stats\n")
     townSelect =(input())
     if(townSelect.isdigit()):
         townSelect = int(townSelect)
+
         if(townSelect == 0):
             town(currMonster)
             continue
+
+        if(townSelect == 1):
+            setMonStats(*miniMonStatMap.get(currMonster, (0, 0, 0)))
+            miniBattleScreen()
+            continue
             
         if(townSelect == 3):
-            monStatMap = {
-                1: (45, 0, 0), #Enemy1
-                }
             setMonStats(*monStatMap.get(currMonster, (0, 0, 0))) #Unpacks and repacks tuple, as well as record enemy stats.
             BattleScreen()
             continue
+
+        if(townSelect == 4):
+            print(stats)
                 
         else:
             print("\nInvalid Input, try again.")
+        
+    else:
+        print("\nInvalid Input, try again.")
+        
+
+#Personality Statements
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your personality is INTJ, imaginative and stragetic thinkers with a plan for everything.")
+
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                    print("Your personality is INTP, innovative investors with a lot of knowledge.")
+
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your personality is ENTJ, bold, imaginative and strong-willed leaders, always finding a way- or making one.") 
+
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your personality is ENTP, smart and curious thinkers who ca not resist an intellectual challenge.")
+
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your personality is INFJ, quiet and mystical, yet very inpiring and tireless idealist.") 
+
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your personality is INFP, poetic, kind and altruistic people, always eager to help a good cause.") 
+
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your personality is ENFJ, charamasitc and inspiring leaders able to mesmerize their listeners.") 
+
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your personality is ENFP, euthusiastic, creative and socialable free spirits, who can always find a reason to smile.")
+
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your personality is ISTJ, practical and fact-minded individuals, whose reliability can not be doubled.") 
+
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your personality is ISFJ, very dedicated and warm protectors, always ready to defend their loved ones.") 
+
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your personality is ESTJ, excellent administrators, unsurpassed at managing things or people.")  
+
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your personality is ESFJ, caring, social, and popular people, always eager to help.") 
+
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your personality is ISTP, bold and practical experimenters") 
+
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your personality is ISFP, flexible and charming artists always ready to explore and experience something new.")
+                
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your personality is ESTP, smart, energentic and very  perceptive people.") 
+
+if(e1Result == 0 and e2Result == 1 and e3Result == 2 and e4Result == 3 and e5Result == 4):
+                print("Your perosnality is ESFP, spontaneous, energetic and enthusiastic people.")
         
     else:
         print("\nInvalid Input, try again.")
